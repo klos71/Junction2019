c3e8d09eaa4c7f40139f77b504ab5de7f826bf6a
klos71 <jonas97_hellman@hotmail.com>
2019-11-15 21:22:08 +0200

    skeleton code for MUI done

--- maintananceUI/src/App.jsx
+++ maintananceUI/src/App.jsx
@@ -6,10 +6,28 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      focus: [60.1855949, 24.8248988],
+      lat: 60.1855949,
+      lng: 24.8248988,
+      zoom: 15,
+      markers: [
+        {
+          name: "test",
+          lat: 60.1859949,
+          lng: 24.8248988,
+          Slots: 10,
+          emptySlots: 4
+        },
+        {
+          name: "test",
+          lat: 60.1895949,
+          lng: 24.8278988,
+          Slots: 15,
+          emptySlots: 22
+        }
+      ]
     };
+    this.focusOnStation = this.focusOnStation.bind(this);
   }
 
   componentDidMount() {
@@ -21,16 +39,42 @@ export default class App extends Component {
     }
   }
 
+  focusOnStation(pos) {
+    console.log(pos);
+    this.setState({ focus: [pos[0], pos[1]] });
+  }
+
   render() {
     const position = [this.state.lat, this.state.lng];
+    let markers = this.state.markers.map((el, index) => {
+      return (
+        <Marker position={[el.lat, el.lng]} key={index}>
+          <Popup>
+            Name: {el.name} <br /> EmptySlots: {el.emptySlots}
+          </Popup>
+        </Marker>
+      );
+    });
+
+    let stations = this.state.markers.map((el, index) => {
+      return (
+        <div key={index} onClick={() => this.focusOnStation([el.lat, el.lng])}>
+          <p>Name: {el.name}</p>
+          <p>EmptySlots:{el.emptySlots}</p>
+        </div>
+      );
+    });
+
     return (
-      <div>
-        <Map center={position} zoom={this.state.zoom}>
+      <div className='flex-container'>
+        <div className='alertContainer'>{stations}</div>
+        <Map center={this.state.focus} zoom={this.state.zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           />
-          <Marker position={position}></Marker>
+
+          {markers}
         </Map>
       </div>
     );
