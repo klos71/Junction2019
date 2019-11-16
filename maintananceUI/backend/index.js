82e802612eced157f0ad747ca648282c25b4135e
klos71 <jonas97_hellman@hotmail.com>
2019-11-16 17:39:11 +0200

    added tokens

--- maintananceUI/backend/index.js
+++ maintananceUI/backend/index.js
@@ -66,8 +66,20 @@ app.get("/user/:user", (req, res) => {
     }
   });
   if (!found) {
-    res.jsonp({ name: req.params.user, id: users.length });
-    users.push({ name: req.params.user, id: users.length });
+    res.jsonp({
+      name: req.params.user,
+      id: users.length,
+      tokens: 0,
+      score: 0,
+      mission: 0
+    });
+    users.push({
+      name: req.params.user,
+      id: users.length,
+      tokens: 0,
+      score: 0,
+      mission: 0
+    });
   }
   fs.writeFileSync(__dirname + "/users.json", JSON.stringify(users));
 });
