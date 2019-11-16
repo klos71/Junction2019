7231d469e74c703b4e3568c4095db24670349885
klos71 <jonas97_hellman@hotmail.com>
2019-11-16 14:44:30 +0200

    test

--- maintananceUI/backend/index.js
+++ maintananceUI/backend/index.js
@@ -6,7 +6,6 @@ const fs = require("fs");
 var stations = fs.readFileSync(__dirname + "/csvjson.json");
 stations = JSON.parse(stations);
 
-app.get("/", (req, res) => res.json(stations));
+app.get("/", (req, res) => res.jsonp(stations));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
