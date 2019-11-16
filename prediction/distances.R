511f5af0375407088296846202850b09ad9582ec
Aggrathon <antonbjo@gmail.com>
2019-11-16 10:04:45 +0200

    fix distance functions

--- prediction/distances.R
+++ prediction/distances.R
@@ -1,26 +1,23 @@
 
-
-# This function takes a dataframe (stations) that contains
-# a column of all the station ids called "id" and returns
-# a list where the first item is a dataframe with two columns
-# "id1" and "id2" of all combinations of ids
+# This function takes a dataframe that contains a column of all
+# the station ids called "id" and returns a dataframe with two
+# columns ("id1" and "id2") with all combinations of ids
 id_permutations <- function(stations, unused) {
-	list(do.call(rbind, lapply(stations$id, function(id) {
+	combinations <- do.call(rbind, lapply(stations$id, function(id) {
 		data.frame(id1=id, id2=stations$id[stations$id != id])
-	})), NULL)
+	}))
+	return(list(dataset1=combinations, dataset2=data.frame()))
 }
 
 
-# This function takes the output from `id_permutations` and
-# The dataframe of all trips and calculates the quantile(dur_qt)
-# duration of the trip and the quantile(dis_qt) distance for each
-# combination (discarding those with
-# too little information). The return is a list where the first
-# item is a dataframe of "DepartureId", "ReturnId",
-# "Duration", and "Distance".
+# This function takes the output from `id_permutations` and the dataframe
+# of all trips. The function calculates the quantile(dur_qt) duration of
+# the trip and the quantile(dis_qt) distance for each combination
+# (discarding those with too little information). The return is a
+# dataframe of "DepartureId", "ReturnId", "Duration", and "Distance".
 calculate_distances <- function(combinations, trips, dur_qt=0.75, dis_qt=0.2) {
-	durs <- c(rep(na, nrow(combinations)))
-	dists <- c(rep(na, nrow(combinations)))
+	durs <- c(rep(NA, nrow(combinations)))
+	dists <- c(rep(NA, nrow(combinations)))
 	for (i in 1:nrow(combinations)) {
 		mask <- which(trips["Departure station id"] == combinations$id1[[i]] & trips["Return station id"] == combinations$id2[[i]])
 		if (length(mask) < 3)
@@ -31,11 +28,11 @@ calculate_distances <- function(combinations, trips, dur_qt=0.75, dis_qt=0.2) {
 		dists[i] <- unname(quantile(lengths, dis_qt))
 	}
 	mask <- !is.na(durs)
-	list(data.frame(
+	return(list(dataset1=data.frame(
 		"DepartureId" = combinations$id1[mask],
 		"ArrivalId" = combinations$id2[mask],
 		"Duration" = durs[mask],
 		"Distance" = dists[mask],
 		check.names = FALSE
-	), NULL)
+	), dataset2=data.frame()))
 }
