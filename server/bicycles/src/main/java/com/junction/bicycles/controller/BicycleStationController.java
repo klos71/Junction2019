1f99452ddb8017ca4afb13b5f507fbaef33730b0
errhal <errhal@gmail.com>
2019-11-16 10:14:27 +0100

    Fixed mapping.

--- server/bicycles/src/main/java/com/junction/bicycles/controller/BicycleStationController.java
+++ server/bicycles/src/main/java/com/junction/bicycles/controller/BicycleStationController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class BicycleStationController {
 
     @Autowired
