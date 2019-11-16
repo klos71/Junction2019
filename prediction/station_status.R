feb788361b1c1b7c19c951cf5cf6f68b66a26444
Aggrathon <antonbjo@gmail.com>
2019-11-16 18:44:24 +0200

    fix station status

--- prediction/station_status.R
+++ prediction/station_status.R
@@ -3,10 +3,10 @@
 read_data_stations <- function(seed = 42) {
 	set.seed(seed)
 	data <- read.csv("data/stations-2019.csv")
-	data.frame(
-		X = data$X, Y = data$Y, id = data$id, name = data$name,
-		total_slot = data$total_slot, bikes = runif(nrow(data), 0, data$total_slot)
-	)[data$operative, ]
+	df <- data.frame(X = data$X, Y = data$Y, id = data$id,
+		name = data$name, total_slot = data$total_slot)[data$operative == "Yes", ]
+	df$bikes <- runif(nrow(df), 0, df$total_slot)
+	df
 }
 
 randomise_bikes <- function(stations) {
