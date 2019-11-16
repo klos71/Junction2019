d32571bea004194281f923223bd98f82ff1ec10f
klos71 <jonas97_hellman@hotmail.com>
2019-11-16 15:39:38 +0200

    more moc

--- maintananceUI/backend/index.js
+++ maintananceUI/backend/index.js
@@ -14,6 +14,49 @@ app.use(cors());
 var stations = fs.readFileSync(__dirname + "/csvjson.json");
 stations = JSON.parse(stations);
 
+var events = {
+  events: [
+    {
+      eventID: 0,
+      orgStation: "539 Aalto-yliopisto (M), Tietotie",
+      orgLat: 60.184987,
+      orgLng: 24.820099,
+      Dstation: "557 Louhentori",
+      Dlat: 60.18715,
+      Dlong: 24.796959
+    }
+  ]
+};
+
+var ongoingEvenets = [];
+
 app.get("/stations", (req, res) => res.jsonp(stations));
+
+app.get("/events", (req, res) => {
+  res.json(events);
+});
+
+app.post("/events", (req, res) => {
+  var name = req.body.name;
+  var eventID = req.body.eventID;
+  var temp = { name: name, eventID: eventID };
+  var found = false;
+  events.events.forEach((el) => {
+    ongoingEvenets.forEach((element) => {
+      console.log(element.eventID);
+      console.log(el.eventID);
+      if (element.eventID === el.eventID) {
+        found = true;
+      }
+    });
+  });
+  if (found) {
+    res.json({ error: "This event has already started" });
+  } else {
+    ongoingEvenets.push(temp);
+    console.log(ongoingEvenets);
+    res.json(temp);
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
